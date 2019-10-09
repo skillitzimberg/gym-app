ea747398d86d9871fa0d29df9909e920db25bebb
Scott Bergler <commandinghands@gmail.com>
2019-10-09 12:37:35 -0700

    separate sidenav-list & header components from app component.

---
+++ src/app/navigation/header/header.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.css']
+})
+export class HeaderComponent implements OnInit {
+  @Output() toggleSidenav = new EventEmitter<void>()
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onToggleSidenav() {
+    this.toggleSidenav.emit();
+  }
+
+}
