05ed591b2561f266efa1035a7ea2137eb75570f4
Scott Bergler <commandinghands@gmail.com>
2019-11-19 12:25:48 -0800

    declare types for State & Action.

--- src/app/app.reducer.ts
+++ src/app/app.reducer.ts
@@ -2,11 +2,15 @@ export interface State {
   isLoading: boolean
 }
 
+export interface Action {
+  type: string
+}
+
 const initialState: State = {
   isLoading: false
 }
 
-export function appReducer(state = initialState, action) {
+export function appReducer(state: State = initialState, action: Action) {
   switch(action.type) {
     case 'START_LOADING':
       return {
